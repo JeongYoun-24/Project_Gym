68e57b35bb5c4adfb25f24e62399b98421636475
jeongYoun-24 <akdntm0302@naver.com>
2023-09-20 15:08:05 +0900

    9/20 저장

---
+++ src/main/java/com/example/gym/entity/Member.java
@@ -0,0 +1,13 @@
+package com.example.gym.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class Member {
+
+    private String MemberID;
+    private String Password;
+
+
+}
